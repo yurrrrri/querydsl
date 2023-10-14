3a3e9d13caa9758ab3d703498dc042da56a9f447
yurison <yu_ri_son@naver.com>
2023-10-14 19:37:23 +0900

    querydsl Init & HelloTest

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,36 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+    }
+
+}
