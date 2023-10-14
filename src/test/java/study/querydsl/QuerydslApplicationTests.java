54634a3b028a25510447cb0d23f1e9aa282d6ec3
yurison <yu_ri_son@naver.com>
2023-10-14 20:05:21 +0900

    예제 도메인 생성 및 테스트

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -12,6 +13,7 @@ import study.querydsl.entity.QHello;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
+@Rollback(value = false)
 @SpringBootTest
 class QuerydslApplicationTests {
 
