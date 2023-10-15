fa014205540c7aa231834e039ec361ad350fdc40
yurison <yu_ri_son@naver.com>
2023-10-15 23:05:45 +0900

    test1

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,63 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void setUp() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    void startJpql() {
+        Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(result.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void startQuerydsl() {
+        queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+        Member result = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(result.getUsername()).isEqualTo("member1");
+    }
+}
